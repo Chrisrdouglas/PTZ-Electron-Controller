aa39c0e04417f408996122e658cb8f91cb32f446
Chris Douglas <chrisr.douglas@yahoo.com>
2021-04-02 12:08:05 -0700

    added power button controlls, config.json file

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const {app, BrowserWindow, ipcMain} = electron;
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 720,
+    resizable: false,
     webPreferences: {
       //preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
